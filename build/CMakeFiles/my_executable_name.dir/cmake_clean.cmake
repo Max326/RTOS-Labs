7757cc2b0831f38d4d48049f0d46be9c18f727de
Max326 <maxconnor326@gmail.com>
2023-11-26 17:29:50 +0100

    initial commit

---
+++ build/CMakeFiles/my_executable_name.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_executable_name.dir/src/main.c.o"
+  "CMakeFiles/my_executable_name.dir/src/main.c.o.d"
+  "my_executable_name"
+  "my_executable_name.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_executable_name.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/my_executable_name.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for my_executable_name.
+# This may be replaced when dependencies are built.

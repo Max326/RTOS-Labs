fa419cd530830a4760cbd0ff05cfd28347ec46c1
Max326 <maxconnor326@gmail.com>
2023-12-03 14:15:35 +0100

    task 2 done

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ubuntu/Projects/RTOS-Labs
+# Install script for directory: /home/max/Projects/RTOS-Labs
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ubuntu/Projects/RTOS-Labs/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/max/Projects/RTOS-Labs/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
-// lab4 assignment 1
+// lab4 assignment 2
 
 #include <sched.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,54 +9,49 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	int fork_pid, fork_pid2, fork_pid3, fork_pid4;
-	int i, j;
-	int status, status2, status3, status4;
+	int fork_pid;
+	int i, res, status;
+
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <path_to_executable> [args_for_executable]\n", argv[0]);
+        return EXIT_FAILURE;
+	}
 
 	fork_pid = fork();
 
 	if(fork_pid == -1) {
-		perror("P: sie nie udao\n");
+		perror("forking failed\n");
+		return EXIT_FAILURE;
 	} else if(fork_pid == 0) {
-		printf("\tR1: potomny pid = %d, macierzysty - P = %d\n", getpid(), getppid());
-		fork_pid2 = fork();
+		printf("pid = %d, ppid = %d\n", getpid(), getppid());
 
-		if(fork_pid2 == -1) {
-			perror("\tR1: sie nie udao\n");
-		} else if(fork_pid2 == 0) {
-			printf("\t\tS1: potomny pid = %d, macierzysty - R1 = %d\n", getpid(), getppid());
-		} else {
-			fork_pid2 = wait(&status2);
+		// res = spawnl(WNOWAIT, "/home/max/Projects/RTOS-exec-test/build/my_executable_name", "my_executable_name", "10", NULL); this should work during labs
+
+		// here's running it without weird stuff:
+		// res = execl("/home/max/Projects/RTOS-exec-test/build/my_executable_name", "my_executable_name", "2", NULL); // except this replaces the whole process with the executable
+
+		printf("Give me the program path and number of steps: ");
+
+		char *program_name = argv[1];
+        char *arg1 = argc > 2 ? argv[2] : NULL;  // First argument for the child program, if provided
+        res = execl(program_name, program_name, arg1, NULL);
+
+		if(res < 0) {
+			printf("error in running the child program\n");
+			exit(EXIT_FAILURE);
 		}
 	} else {
-		printf("P: proces macierzysty pid = %d\n", getpid());
+		printf("parent pid = %d\n", getpid());
 
-		fork_pid3 = fork();
-
-		if(fork_pid3 == -1) {
-			perror("P: sie nie udalo stworzyc R2\n");
-		} else if(fork_pid3 == 0) {
-			printf("\tR2: potomny pid = %d, macierzysty - P %d\n", getpid(), getppid());
-
-			fork_pid4 = fork();
-
-			if(fork_pid4 == -1) {
-				perror("R2: sie nie udalo stworzyc S2\n");
-			} else if(fork_pid4 == 0) {
-				printf("\t\tS2: potomny pid = %d, macierzysty - R2 = %d\n", getpid(), getppid());
-			} else{
-				fork_pid4 = wait(&status4);
-			}
-		} else{
-			fork_pid3 = wait(&status3);
+		for(i = 1; i <= 3; i++) {
+			printf("Parent - step %d\n", i);
+			sleep(1);
 		}
 
-		printf("P: Czekam na potomny ...\n");
 		fork_pid = wait(&status);
-
-		printf("P: Proces potomny PID = %d zakonczony; status : %d\n", fork_pid, WEXITSTATUS(status));
+		printf("parent process pid = %d finished\n", getpid());
 	}
-	printf(" Proces PID = %d konczy prace \n", getpid());
+	printf("proces pid = %d finished working, status = %d\n", getpid(), status);
 
 	return 0;
 }
